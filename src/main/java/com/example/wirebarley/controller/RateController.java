538ade446db5154ed082118c9a808844b1bf7c3a
Lokie89 <traeuman@gmail.com>
2020-11-18 09:28:18 +0900

    init

---
+++ src/main/java/com/example/wirebarley/controller/RateController.java
@@ -0,0 +1,13 @@
+package com.example.wirebarley.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RateController {
+    @GetMapping("/rate")
+    public void rate(){
+
+    }
+
+}
