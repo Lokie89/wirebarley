241f5fe495cfb1c1fb99af27c053b90fefbf2991
Lokie89 <traeuman@gmail.com>
2020-11-20 11:53:11 +0900

    fix : exception handler to rest

--- src/main/java/com/example/wirebarley/controller/ExceptionHandler.java
+++ src/main/java/com/example/wirebarley/controller/ExceptionHandler.java
@@ -2,10 +2,10 @@ package com.example.wirebarley.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandler {
     @ResponseBody
     @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
